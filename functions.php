252a287c6ec351149d9b699f2ad66c419beeb822
Ryan Miller <ryansmiller@gmail.com>
2017-05-09 21:20:26 -0400

    Add Fade In to Navbar

--- functions.php
+++ functions.php
@@ -114,8 +114,10 @@ function kyle_westaway_minimal_scripts() {
 	wp_enqueue_style( 'kyle-westaway-minimal-style', get_stylesheet_uri() );
 
 	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.3.7', false );
-	
+
 	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '3.3.7', true );
+
+	wp_enqueue_script( 'custom', get_template_directory_uri() . '/js/custom.js', array('bootstrap'), null, true );
 
 }
 add_action( 'wp_enqueue_scripts', 'kyle_westaway_minimal_scripts' );
---
+++ js/custom.js
@@ -0,0 +1,22 @@
+(function ($) {
+  $(document).ready(function(){
+    
+	// hide .navbar first
+	$(".home .navbar").hide();
+	
+	// fade in .navbar
+	$(function () {
+		$(window).scroll(function () {
+            // set distance user needs to scroll before we fadeIn navbar
+			if ($(this).scrollTop() > 100) {
+				$('.home .navbar').fadeIn();
+			} else {
+				$('.home .navbar').fadeOut();
+			}
+		});
+
+	
+	});
+
+});
+  }(jQuery));
