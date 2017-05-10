25f149a8cb04661e966f49a614592e29c5921b6b
Ryan Miller <ryansmiller@gmail.com>
2017-05-09 22:59:15 -0400

    Add Font Awesome

--- functions.php
+++ functions.php
@@ -137,6 +137,8 @@ function kyle_westaway_minimal_scripts() {
 
 	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.3.7', false );
 
+	wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/css/font-awesome.min.css', array(), '4.7.0', false );
+
 	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '3.3.7', true );
 
 	wp_enqueue_script( 'custom', get_template_directory_uri() . '/js/custom.js', array('bootstrap'), null, true );
